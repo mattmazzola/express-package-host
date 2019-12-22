4b44c45401b38ed10e7d189446eed7508c911dc4
Matt Mazzola <mattmazzola@live.com>
2019-12-22 15:01:09 -0800

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import path from 'path'
+
+const app = express()
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+app.listen(9000)
